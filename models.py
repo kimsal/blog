129687d4aee6578fa225227a2f9c01a6365be560
rithythul <rithythul@gmail.com>
2016-07-14 11:31:24 +0700

    add

--- models.py
+++ models.py
@@ -4,9 +4,7 @@ from slugify import slugify
 from wtforms.widgets import * #TextArea
 from wtforms import * #TextField, IntegerField, TextAreaField, SubmitField, RadioField,SelectField,validators, ValidationError
 import wtforms.widgets.core
-
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
-
 class UserMember(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50))
@@ -33,9 +31,6 @@ class UserMember(db.Model):
     def delete(user):
         db.session.delete(user)
         return session_commit()
-    # def generate_auth_token(self, expiration = 600000):
-    #     s = Serializer(SECRET_KEY, expires_in = expiration)
-    #     return s.dumps({ 'password': self.password })
     @staticmethod
     def verify_auth_token(token):
         s = Serializer(SECRET_KEY)
@@ -68,10 +63,6 @@ class Category(db.Model):
     def add(category):
         db.session.add(category)
         return db.session.commit()
-    # def update(category,name):
-    #     category.update({"slug" : slugify(name) , "name" : dict(name)})
-    #     #db.session.commit()
-    #     return session_commit()
     def delete(category):
         db.session.delete(category)
         return db.session.commit()
@@ -100,8 +91,6 @@ class Page(db.Model):
     def add(page):
         db.session.add(page)
         return db.session.commit()
-    # def update(self):
-    #     return session_commit()
     def delete(page):
         db.session.delete(page)
         return db.session.commit()
@@ -115,16 +104,16 @@ class Post(db.Model):
     user_id=db.Column(db.Integer,db.ForeignKey('user_member.id'))
     published_at=db.Column(db.TIMESTAMP,server_default=db.func.current_timestamp())
     views = db.Column(db.Integer, nullable=True)
-    # def to_Json(self):
-    #     return dict(id=self.id,
-    #         title=self.title,
-    #         description=self.description,
-    #         feature_image=self.feature_image,
-    #         slug=self.slug,
-    #         category_id=self.category_id,
-    #         published_at="{}".format(self.published_at),
-    #         view=self.view
-    #         )
+    def to_Json(self):
+        return dict(id=self.id,
+            title=self.title,
+            description=self.description,
+            feature_image=self.feature_image,
+            slug=self.slug,
+            category_id=self.category_id,
+            published_at="{}".format(self.published_at),
+            view=self.view
+            )
     def __init__(self, title, description, category_id, feature_image, user_id,views=0):
         self.title = title
         self.description = description
@@ -141,50 +130,76 @@ class Post(db.Model):
     def delete(post):
         db.session.delete(post)
         return db.session.commit()
-
-
-#http://flask-admin.readthedocs.io/en/latest/advanced/
-
-#http://charlesleifer.com/blog/how-to-make-a-flask-blog-in-one-hour-or-less/
-
-
-        
-    # def __init__(title, description,feature_image, slug,category,published_at):
-    #         self.title = title
-    #         self.description = Database
-    #         self.feature_image=feature_image
-    #         self.slug=slug
-    #         self.category=category
-    #         self.published_at=published_at
-    #     def add(self,post):
-    #         db.session.add(post)
-    #         return session_commit()
-    #     def update(self):
-    #         return session_commit()
-    #     def delete(self,post):
-    #         db.session.delete(post)
-    #         return session_commit()
-    # def session_commit():
-    #     try:
-    #         db.session.commit()
-    #     except SQLAlchemyError as e:
-    #         reason=str(e)
-
+class Email(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(255),unique=True)
+    name  = db.Column(db.String(255),nullable=True)
+    published_at=db.Column(db.TIMESTAMP,server_default=db.func.current_timestamp())
+    def __str__(self):
+        return self.name
+    def to_Json(self):
+        return dict(id=self.id,
+            email=self.email,
+            name=self.name
+            )
+    def __init__(self, email,name):
+        self.email = email
+        self.name =name
+    def add(email):
+        db.session.add(email)
+        return db.session.commit()
+    def delete(email):
+        db.session.delete(email)
+        return db.session.commit()
+class Group(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name  = db.Column(db.String(255),nullable=True)
+    published_at=db.Column(db.TIMESTAMP,server_default=db.func.current_timestamp())
+    def __str__(self):
+        return self.name
+    def to_Json(self):
+        return dict(id=self.id,
+            name=self.name
+            )
+    def __init__(self,name):
+        self.name =name
+    def add(group):
+        db.session.add(group)
+        return db.session.commit()
+    def delete(group):
+        db.session.delete(group)
+        return db.session.commit()
+class Emailgroup(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    email_id  = db.Column(db.Integer)
+    group_id  = db.Column(db.Integer)
+    published_at=db.Column(db.TIMESTAMP,server_default=db.func.current_timestamp())
+    def __str__(self):
+        return self.email_id
+    def to_Json(self):
+        return dict(id=self.id,
+            email_id=self.email_id,
+            group_id=self.group_id
+            )
+    def __init__(self,email_id,group_id):
+        self.email_id =email_id,
+        self.group_id =group_id
+    def add(emailgroup):
+        db.session.add(emailgroup)
+        return db.session.commit()
+    def delete(emailgroup):
+        db.session.delete(emailgroup)
+        return db.session.commit()
 #need when migrate database 
 if __name__ == '__main__':
     app.secret_key = SECRET_KEY
     app.config['DEBUG'] = True
     app.config['SESSION_TYPE'] = 'filesystem'
-   # sess.init_app(app)
     app.debug = True
     manager.run()
     app.run()
-
-
-
 # @manager.command
 # def init_db():
 #     db.drop_all()
 #     db.create_all()
-
 #http://techarena51.com/index.php/one-to-many-relationships-with-flask-sqlalchemy/
